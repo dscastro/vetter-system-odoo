d450f43ab3f47312dbcc7830487b6ea80225b17e
Diony S. Castro <dnycstr@gmail.com>
2021-02-17 14:02:19 +0800

    Update enddate

--- dsc_brandmodule/models/models.py
+++ dsc_brandmodule/models/models.py
@@ -7,13 +7,13 @@ class dsc_brandmodule(models.Model):
     _name = 'dsc_brandmodule.dsc_brandmodule'
     _description = 'dsc_brandmodule.dsc_brandmodule'
 
-    name = fields.Char()
+    name = fields.Char()    
     value = fields.Integer()
     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
     start_datetime = fields.Datetime('Start time', default=lambda self: fields.Datetime.now())
-
-
+    end_datetime = fields.Datetime('End time', default=lambda self: fields.Datetime.now())
+    
     @api.depends('value')
     def _value_pc(self):
         for record in self:
