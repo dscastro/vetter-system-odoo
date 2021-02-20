cf28152afa459313cde57bf100966886584912f3
Diony S. Castro <dnycstr@gmail.com>
2021-02-21 02:15:41 +0800

    Remove VSI Module

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -20,7 +20,7 @@ from odoo import models, fields, api
 
 class Course(models.Model):
     _name = 'openacademy.course'
-    _description = 'OpenAcademy Course'
+    _description = 'OpenAcademy Courses'
 
     name = fields.Char(string="Title", required=True)
     description =fields.Text()
